87f17d8b3afbad830937f85cd0a4f00aa1501ef1
Eric Ahlberg <eahlberg@gmail.com>
2016-04-21 20:28:04 +0200

    Change cluster representation and function naming
    - Return cluster as lists of lists
    - Use underscore on non-API functions

--- gdbscan.py
+++ gdbscan.py
@@ -1,25 +1,34 @@
+import copy
+
 UNCLASSIFIED = -2
 NOISE = -1
 
 
 def GDBSCAN(objects, n_pred, min_card, w_card):
-    cluster_id = NOISE + 1
-    for i in range(len(objects) - 1):
-        obj = objects.get(i)
+    objects = copy.deepcopy(objects)
+    cluster_id = 0
+    for obj in objects:
         if obj.cluster_id == UNCLASSIFIED:
-            if expand_cluster(objects, obj, cluster_id, n_pred, min_card,
-                              w_card):
+            if _expand_cluster(objects, obj, cluster_id, n_pred, min_card,
+                               w_card):
                 cluster_id = cluster_id + 1
-    return objects
+    clusters = {}
+    for obj in objects:
+        key = obj.cluster_id
+        if key in clusters:
+            clusters[key].append(obj)
+        else:
+            clusters[key] = [obj]
+    return list(clusters.itervalues())
 
 
-def expand_cluster(objects, obj, cluster_id, n_pred, min_card, w_card):
-    if not in_selection(w_card, obj):
+def _expand_cluster(objects, obj, cluster_id, n_pred, min_card, w_card):
+    if not _in_selection(w_card, obj):
         objects.change_cluster_id(obj, UNCLASSIFIED)
         return False
 
     seeds = objects.neighborhood(obj, n_pred)
-    if not core_object(w_card, min_card, seeds):
+    if not _core_object(w_card, min_card, seeds):
         objects.change_cluster_id(obj, NOISE)
         return False
 
@@ -39,30 +48,27 @@ def expand_cluster(objects, obj, cluster_id, n_pred, min_card, w_card):
     return True
 
 
-def in_selection(w_card, obj):
+def _in_selection(w_card, obj):
     return w_card([obj]) > 0
 
 
-def core_object(w_card, min_card, objects):
+def _core_object(w_card, min_card, objects):
     return w_card(objects) >= min_card
-
-
-def w_card(objects):
-    return len(objects)
 
 
 class Objects:
     def __init__(self, objects):
         self.objects = objects
 
-    def get(self, index):
-        return self.objects[index]
-
-    def __len__(self):
-        return len(self.objects)
+    def __iter__(self):
+        for obj in self.objects:
+            yield obj
 
     def __repr__(self):
         return str(self.objects)
+
+    def get(self, index):
+        return self.objects[index]
 
     def neighborhood(self, obj, n_pred):
         return filter(lambda x: n_pred(obj, x), self.objects)
@@ -72,5 +78,8 @@ class Objects:
             self.change_cluster_id(obj, value)
 
     def change_cluster_id(self, obj, value):
-        index = self.objects.index(obj)
+        index = (self.objects).index(obj)
         self.objects[index].cluster_id = value
+
+    def labels(self):
+        return set(map(lambda x: x.cluster_id, self.objects))
